8d382b042dacc44999c0a45403a7b3b827c8054d
Dylan Ashley <dylanashley@dylanashley.io>
2017-12-07 13:51:17 -0700

    changed plot to use mean error of a timestep

--- plot.py
+++ plot.py
@@ -28,6 +28,16 @@ for i in range(200):
     for j in range(NUMBER_OF_EPISODES):
         k_distance_based_exploration_count[i, j] = np.sum(
             k_distance_based_exploration[i, j, :, :])
+for i in reversed(range(1, NUMBER_OF_EPISODES)):
+    k_distance_based_exploration_std[:,
+                                     i] -= k_distance_based_exploration_std[:,
+                                                                            i -
+                                                                            1]
+    k_distance_based_exploration_count[:,
+                                       i] -= k_distance_based_exploration_count[:,
+                                                                                i
+                                                                                -
+                                                                                1]
 
 # load LOF based exploration results
 lof_based_exploration = np.zeros((200, NUMBER_OF_EPISODES, 5, 5)) * np.nan
@@ -48,6 +58,9 @@ for i in range(200):
     for j in range(NUMBER_OF_EPISODES):
         lof_based_exploration_count[i, j] = np.sum(
             lof_based_exploration[i, j, :, :])
+for i in reversed(range(1, NUMBER_OF_EPISODES)):
+    lof_based_exploration_std[:, i] -= lof_based_exploration_std[:, i - 1]
+    lof_based_exploration_count[:, i] -= lof_based_exploration_count[:, i - 1]
 
 # load policy iteration results
 policy_iteration = np.zeros((1000, NUMBER_OF_EPISODES, 5, 5)) * np.nan
@@ -65,6 +78,9 @@ policy_iteration_count = np.zeros((1000, NUMBER_OF_EPISODES)) * np.nan
 for i in range(1000):
     for j in range(NUMBER_OF_EPISODES):
         policy_iteration_count[i, j] = np.sum(policy_iteration[i, j, :, :])
+for i in reversed(range(1, NUMBER_OF_EPISODES)):
+    policy_iteration_std[:, i] -= policy_iteration_std[:, i - 1]
+    policy_iteration_count[:, i] -= policy_iteration_count[:, i - 1]
 
 # load random exploration results
 random_exploration = np.zeros((1000, NUMBER_OF_EPISODES, 5, 5)) * np.nan
@@ -83,65 +99,55 @@ random_exploration_count = np.zeros((1000, NUMBER_OF_EPISODES)) * np.nan
 for i in range(1000):
     for j in range(NUMBER_OF_EPISODES):
         random_exploration_count[i, j] = np.sum(random_exploration[i, j, :, :])
+for i in reversed(range(1, NUMBER_OF_EPISODES)):
+    random_exploration_std[:, i] -= random_exploration_std[:, i - 1]
+    random_exploration_count[:, i] -= random_exploration_count[:, i - 1]
 
 # setup plot
 sns.set_style("whitegrid")
-plt.gcf().set_size_inches(10, 5)
+plt.gcf().set_size_inches(5, 5)
 plt.gcf().set_dpi(600)
 
 # plot error over episodes
-ax = plt.subplot(1, 2, 1)
 episodes = np.arange(NUMBER_OF_EPISODES) + 1
-ax.errorbar(
+plt.errorbar(
     episodes,
-    np.nanmean(k_distance_based_exploration_std, axis=0),
-    yerr=st.sem(k_distance_based_exploration_std, axis=0, nan_policy='omit'),
+    np.nanmean(
+        k_distance_based_exploration_std / k_distance_based_exploration_count,
+        axis=0),
+    yerr=st.sem(
+        k_distance_based_exploration_std / k_distance_based_exploration_count,
+        axis=0,
+        nan_policy='omit'),
     label='k-distance')
-ax.errorbar(
+plt.errorbar(
     episodes,
-    np.nanmean(lof_based_exploration_std, axis=0),
-    yerr=st.sem(lof_based_exploration_std, axis=0, nan_policy='omit'),
+    np.nanmean(
+        lof_based_exploration_std / lof_based_exploration_count, axis=0),
+    yerr=st.sem(
+        lof_based_exploration_std / lof_based_exploration_count,
+        axis=0,
+        nan_policy='omit'),
     label='LOF')
-ax.errorbar(
+plt.errorbar(
     episodes,
-    np.nanmean(policy_iteration_std, axis=0),
-    yerr=st.sem(policy_iteration_std, axis=0, nan_policy='omit'),
+    np.nanmean(policy_iteration_std / policy_iteration_count, axis=0),
+    yerr=st.sem(
+        policy_iteration_std / policy_iteration_count,
+        axis=0,
+        nan_policy='omit'),
     label='Policy Iteration')
-ax.errorbar(
+plt.errorbar(
     episodes,
-    np.nanmean(random_exploration_std, axis=0),
-    yerr=st.sem(random_exploration_std, axis=0, nan_policy='omit'),
+    np.nanmean(random_exploration_std / random_exploration_count, axis=0),
+    yerr=st.sem(
+        random_exploration_std / random_exploration_count,
+        axis=0,
+        nan_policy='omit'),
     label='Random')
-ax.set_xlabel('Episode')
-ax.set_ylabel('Standard Deviation')
-ax.legend(loc='best')
-
-# plot timesteps per episode over time
-ax = plt.subplot(1, 2, 2)
-episodes = np.arange(NUMBER_OF_EPISODES) + 1
-ax.errorbar(
-    episodes,
-    np.nanmean(k_distance_based_exploration_count, axis=0),
-    yerr=st.sem(k_distance_based_exploration_count, axis=0, nan_policy='omit'),
-    label='k-distance')
-ax.errorbar(
-    episodes,
-    np.nanmean(lof_based_exploration_count, axis=0),
-    yerr=st.sem(lof_based_exploration_count, axis=0, nan_policy='omit'),
-    label='LOF')
-ax.errorbar(
-    episodes,
-    np.nanmean(policy_iteration_count, axis=0),
-    yerr=st.sem(policy_iteration_count, axis=0, nan_policy='omit'),
-    label='Policy Iteration')
-ax.errorbar(
-    episodes,
-    np.nanmean(random_exploration_count, axis=0),
-    yerr=st.sem(random_exploration_count, axis=0, nan_policy='omit'),
-    label='Random')
-ax.set_xlabel('Episode')
-ax.set_ylabel('Steps')
-ax.legend(loc='best')
+plt.xlabel('Episode')
+plt.ylabel('Mean Error of a Timestep')
+plt.legend(loc='best')
 
 plt.tight_layout()
 plt.savefig('results.png')
